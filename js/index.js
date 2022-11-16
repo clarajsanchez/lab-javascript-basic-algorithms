f839410168922554bfec7f9c078f42d9b744f33b
clarajsanchez <clarajsanchez@hotmail.com>
2022-11-16 13:32:53 +0100

    lab basic algorithms

--- js/index.js
+++ js/index.js
@@ -53,3 +53,18 @@ else if (compare === 1) {
 else if (compare === 0) {
     console.log("What?! You both have the same name?");
 }
+
+// Bonus 1
+let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at enim magna. Phasellus nec mollis nunc. Aliquam porta dignissim felis, sed fringilla diam vehicula eget. Nam sit amet tempor diam, vitae blandit magna. Suspendisse quam mauris, auctor ut ex vel, venenatis iaculis enim. Maecenas non pretium ex. In tortor justo, luctus eget velit eget, ultrices consectetur purus. Ut condimentum, quam in molestie suscipit, leo purus vehicula ante, quis ultrices ante ipsum tristique sapien. Ut luctus odio sit amet ipsum cursus faucibus. Vivamus fringilla nisl nunc. Cras id massa ac est aliquam suscipit. Nam vitae volutpat magna, eu ornare mi. Curabitur sit amet accumsan massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur aliquet, dolor quis bibendum tincidunt, dolor ipsum elementum lacus, nec malesuada metus leo at diam. Vestibulum bibendum fringilla felis, vitae tincidunt libero tristique vel. Curabitur sed tortor sed est gravida elementum. Etiam posuere eros nec quam dignissim accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque ullamcorper tellus a diam dapibus tincidunt. Cras lobortis risus sed hendrerit rhoncus. Fusce id maximus elit. Donec ornare, dolor vitae varius sagittis, urna erat ultrices tortor, at aliquet lorem ante at augue. Fusce purus lacus, vehicula vel iaculis quis, imperdiet non lectus. Nulla maximus justo nec elit viverra, eu faucibus lorem pharetra. In non dignissim dolor. Pellentesque neque turpis, dictum ut nunc vel, efficitur ultrices nibh. Praesent at risus non urna venenatis bibendum. Vivamus hendrerit magna nisi, sit amet aliquam magna pellentesque sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent imperdiet urna ac lacus vulputate facilisis.";
+
+let counter = 1;
+let etCounter = 0;
+for (let i = 0; i < lorem.length; i++){
+    if (lorem[i] === " ") {
+        counter++;
+    }
+    if (lorem[i] === " " && lorem[i+1] === "e" && lorem[i+2] ==="t" && lorem[i+3] === " ") {
+        etCounter++;
+    } 
+}
+console.log(`Counter: ${counter}, et counter: ${etCounter}`);
